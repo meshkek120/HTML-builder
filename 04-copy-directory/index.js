04cb5b5ecb39d353932e264232ecdfe15665aa0c
newfolder.rar <mdouzhyk@gmail.com>
2022-11-08 03:14:27 +0400

    feat: 04-copy-directory finished

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const fs = require('fs');
 
+
+const originalPath = path.join(__dirname, 'files')
+
+
+//Create an empty folder
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) throw err;
+    console.log('Folder has been copied');
+});
+
+const copiedPath = path.join(__dirname, 'files-copy')
+
+
+//Copy original files
+fs.readdir(originalPath, function (err, files) {
+    if (err) {
+        console.log(err);
+    } 
+    files.forEach(function (file) {
+       const originalFilePath = originalPath + '/' + file,
+        copiedFilePath = copiedPath + '/' + file
+
+        fs.cp(originalFilePath, copiedFilePath, { recursive: true }, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+});
